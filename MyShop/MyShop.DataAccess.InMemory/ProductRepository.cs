181472c1d52c1e31d2a39bd7198a78701e4fbb76
Shawn Weber <shawn.weber28@gmail.com>
2020-09-04 11:34:10 -0400

    Product Category Manager Built and Tested

--- MyShop/MyShop.DataAccess.InMemory/ProductRepository.cs
+++ MyShop/MyShop.DataAccess.InMemory/ProductRepository.cs
@@ -12,7 +12,7 @@ namespace MyShop.DataAccess.InMemory
     public class ProductRepository
     {
         ObjectCache cache = MemoryCache.Default;
-        List<Product> products = new List<Product>();
+        List<Product> products;
 
         public ProductRepository()
         {
